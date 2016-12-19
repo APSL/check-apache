52ff608201ca341a5c70d5e9ca185169f5f7ce57
root <root@pbr-pre2.localdomain>
2016-12-19 17:43:30 +0000

    Check apache

--- check_apache/main.py
+++ check_apache/main.py
@@ -8,8 +8,8 @@
 import logging
 import click
 import sys
-import dns.resolver
-from netaddr import *
+import requests
+
 
 NAGIOS_CODES = {
     'OK': 0,
@@ -22,10 +22,17 @@ NAGIOS_CODES = {
 
 @click.command()
 #@click.option('--network', '-n', multiple=True)
-#@click.option('--domain', '-d')
-def check():
+@click.option('--busy_workers', '-b')
+def check(busy_workers):
   # curl http://127.0.0.1/server-status?auto
-  print("curl apache-status?auto") 
+  r = requests.get("http://127.0.0.1/server-status?auto")
+  for l in r.iter_lines():
+    attr, value = l.split(": ")
+    if attr == 'BusyWorkers':
+      print('Busy: {}'.format(value))
+      if value > busy_workers:
+        print('Error: Num. de workers_busy: {} - Por encima del umbral: {}'.format(value, busy_workers))
+        sys.exit(NAGIOS_CODES['CRITICAL'])
 
   #if KO:
   #    print("Error: La Ip {} del dominio {} NO pertenece a ninguna red {}.".format(ip_domain, domain, network))
